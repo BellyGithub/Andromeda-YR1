0078f57f710a4fd5f50b774cab4e8df90bf0627b
haniowx <honeyhuang2002@gmail.com>
2024-11-17 21:55:52 -0500

    fill health bar issur

--- Assets/Scripts/Testing/HealthManager.cs
+++ Assets/Scripts/Testing/HealthManager.cs
@@ -21,17 +21,12 @@ public class HealthManager : MonoBehaviour
 
         if (currentHealth <= 0)
         {
-            Die();
+            Respawn();
         }
         else
         {
             StartCoroutine(InvincibilityCoroutine());
         }
-    }
-    public void Die()
-    {
-        Debug.Log("You die");
-        Respawn();
     }
 
     public void RefreshHealthbar()
@@ -42,9 +37,11 @@ public class HealthManager : MonoBehaviour
 
     public void Respawn()
     {
+        Debug.Log("You die");
         transform.position = initialPosition;
         currentHealth = 100.0f;
         Debug.Log("Character has respawned at the initial position!");
+        RefreshHealthbar();
     }
 
     IEnumerator InvincibilityCoroutine()
