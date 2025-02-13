8332cc1ddeda29ee6bd8bb3d9931883a447534a1
haniowx <honeyhuang2002@gmail.com>
2025-02-13 14:58:45 -0500

    Add time score and trigger after level ends

--- Assets/Scripts/Testing/Score.cs
+++ Assets/Scripts/Testing/Score.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 public class Score : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
-    public int score = 0;
+    private float startTime;
+    private int score = 0;
+    public void LevelCompleted()
+    {
+        int initialScore = 100000;
+        float elapsedTime = Time.time - startTime;
+        int penalty = Mathf.FloorToInt(elapsedTime) * 1000;
+        score += Mathf.Max(0, initialScore - penalty);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        startTime = Time.time;
         UpdateScoreUI();
     }
 
