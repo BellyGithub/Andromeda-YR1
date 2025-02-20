7629d13514566d0b24a2b72a43cbacc39ca93439
Colin Bennett <colin.bennett079@yahoo.ca>
2025-02-20 00:39:58 -0500

    Level 2 finished, added finishing door

--- Assets/Scripts/Testing/FinishTrigger.cs
+++ Assets/Scripts/Testing/FinishTrigger.cs
@@ -2,30 +2,21 @@ using UnityEngine;
 
 public class FinishTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] private string sceneName;
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Score scoreManager = FindObjectOfType<Score>();
             if (scoreManager != null)
             {
-                scoreManager.LevelCompleted();
+                Debug.Log("Loading next level... beep boop...");
+                scoreManager.LevelCompleted(sceneName);
             }
             else
             {
                 Debug.LogWarning("ScoreManager not found");
             }
         }
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/Testing/Score.cs
+++ Assets/Scripts/Testing/Score.cs
@@ -1,16 +1,19 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     private float startTime;
     private int score = 0;
-    public void LevelCompleted()
+
+    public void LevelCompleted(string sceneName)
     {
         int initialScore = 100000;
         float elapsedTime = Time.time - startTime;
         int penalty = Mathf.FloorToInt(elapsedTime) * 1000;
+        SceneManager.LoadScene(sceneName);
         score += Mathf.Max(0, initialScore - penalty);
     }
 
