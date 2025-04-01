8d0c1b361c429442b53cb1d9c79d029efd333a4c
PadalingamVelmurugan <200600024@student.georgianc.on.ca>
2025-04-01 18:00:58 -0400

    Boss_Level_Progress

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,11 +4,11 @@ public class CameraFollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
     public Transform target;
+    public float yOffset = 2f; // Offset value to move the camera upwards
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
